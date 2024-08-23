9e15fc19fe4dbcb7bfd80d062e4d147944e526d3
FEASGrad <feasgrad@gmail.com>
2024-08-23 15:31:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-<script src="https://cdn.botpress.cloud/webchat/v2.1/inject.js"></script>
-<script src="https://mediafiles.botpress.cloud/67966669-99ab-4915-afd5-a12e0c572c5c/webchat/v2.1/config.js"></script>
+
